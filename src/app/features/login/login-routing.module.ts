fb3213e5f5df6959a6db3001607e03fd5d384612
André Alves Pereira <andre.alves.pereira06@aluno.ifce.edu.br>
2024-04-01 15:23:50 -0300

    :sparkles: feat: update

--- src/app/features/login/login-routing.module.ts
+++ src/app/features/login/login-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "src/app/features/login/login.component";
+import { LoginComponent } from 'src/app/features/login/login.component';
 
 const routes: Routes = [
   {
