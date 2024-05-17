49551cd29b33f71db60c8ced46cbf8928be0d504
Andre Alves <andrealves10a@gmail.com>
2024-05-17 19:51:50 -0300

    :wrench: refactor: update

--- src/app/core/components/logo/logo.component.ts
+++ src/app/core/components/logo/logo.component.ts
@@ -1,10 +1,14 @@
-import {booleanAttribute, Component, Input} from '@angular/core';
+import { booleanAttribute, Component, input } from '@angular/core';
+import { CheckedComponent } from './svg/checked/checked.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-logo',
+  standalone: true,
+  imports: [NgClass, CheckedComponent],
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss']
+  styleUrls: ['./logo.component.scss'],
 })
 export class LogoComponent {
-  @Input({transform: booleanAttribute }) colorful = false;
+  colorful = input(false, { transform: booleanAttribute });
 }
