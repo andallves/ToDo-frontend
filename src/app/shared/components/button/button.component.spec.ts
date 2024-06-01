a6e774f4c90057d7a025b7b5095880b309653cd8
Andre Alves <andrealves10a@gmail.com>
2024-06-01 19:02:45 -0300

    test: implements button test

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe(ButtonComponent.name, () => {
   let component: ButtonComponent;
@@ -24,11 +25,20 @@ describe(ButtonComponent.name, () => {
     expect(component).toBeTruthy();
   });
 
+  it('Should render with passed aria-label and button text when called', () => {
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(
+      By.css('.btn-submit')
+    ).nativeElement;
+    const ariaLabel = buttonElement.getAttribute('aria-label');
+    expect(ariaLabel).toContain('aria test');
+  });
+
   it(`#${ButtonComponent.prototype.onClick.name}
     should trigger (@Output onClicked) when called`, () => {
-    spyOn(component.onClicked, 'emit');
+    const spy = spyOn(component.onClicked, 'emit');
     fixture.detectChanges();
     component.onClick();
-    expect(component.onClicked.emit).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
 });
