eb5405dd1dcfe38ebee0932a2f01578fff1136c6
Andre Alves <andrealves10a@gmail.com>
2024-06-03 22:35:05 -0300

    :sparkles: feat: criando um interceptor para autenticação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,20 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
     provideToastr(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
 };
---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { AuthTokenService } from '../../shared/services/auth-token.service';
+import {
+  HttpEvent,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> => {
+  const authToken = inject(AuthTokenService).getAuthToken();
+  const newReq = req.clone({
+    headers: req.headers.set('Authentication: Bearer', authToken),
+  });
+
+  return next(newReq);
+};
