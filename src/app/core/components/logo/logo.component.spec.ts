96e48ea303a468e67dc2956aaf12b7ac77135eec
Andre Alves <andrealves10a@gmail.com>
2024-06-12 21:26:04 -0300

    🧪 test: testando o input do componente logo

--- src/app/core/components/logo/logo.component.spec.ts
+++ src/app/core/components/logo/logo.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { LogoComponent } from './logo.component';
 
@@ -10,12 +11,23 @@ describe('LogoComponent', () => {
     TestBed.configureTestingModule({
       imports: [LogoComponent],
     });
+    const colorful = true;
     fixture = TestBed.createComponent(LogoComponent);
+    fixture.componentRef.setInput('colorful', colorful);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('Shoud be colorful when called with isColorful equal true', () => {
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(
+      By.css('span')
+    ).nativeElement;
+    const className = buttonElement.getAttribute('class');
+    expect(className).toContain('colorful');
   });
 });
