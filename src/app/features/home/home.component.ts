f4a8a6c50cd2dbb306891d047e4242a58b6c9f11
Andre Alves <andrealves10a@gmail.com>
2024-06-12 21:28:09 -0300

    :sparkles: feat: criando o componente home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/core/models/user.interface';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  protected user = signal<User | null>(null);
+  private userService = inject(UserService);
+  private router = inject(Router);
 
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: value => this.user.set(value),
+    });
+  }
+
+  public logout(): void {
+    this.userService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
